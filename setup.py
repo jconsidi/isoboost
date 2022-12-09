f45df8a208a3bc1b29de49953f6e00cb6d0df79f
Jeffrey Considine <jconsidi@gmail.com>
2022-12-08 21:08:32 -0500

    + fix setup requirement to use scikit-learn instead of sklearn

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from distutils.core import setup
 setup(name = 'isoboost',
       version = '0.1',
       packages = ['isoboost'],
-      install_requires=['sklearn'],
+      install_requires=['scikit-learn'],
       )
